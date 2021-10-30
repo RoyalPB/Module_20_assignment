466243da99da7c9c384b15031430777839dbf987
Philip Burton <68266107+RoyalPB@users.noreply.github.com>
2021-10-30 00:51:05 -0400

    Add files via upload

---
+++ hw_m20_joint_savings/README.md
@@ -0,0 +1,33 @@
+# hw_m20_joint_savings
+
+This main file in this repo, joint_savings.sol, is a Solitidy smart contract that automates the creation of joint savings accounts. It accepts two user addresses that are able to control the account by making deposits and withdrawals.
+
+![Contract overview](Execution_Results/1.PNG)
+
+Here is the deployed contract:
+
+![Deployed contract](Execution_Results/2.PNG)
+
+The following are examples of the functionality of the contract:
+
+ - Set accounts
+
+![Set accounts](Execution_Results/3.PNG)
+
+ - Deposits
+
+![Deposit 1](Execution_Results/4.PNG)
+
+![Deposit 2](Execution_Results/5.PNG)
+
+![Deposit 3](Execution_Results/6.PNG)
+
+ - Withdrawals
+
+![Withdrawal 1](Execution_Results/7.PNG)
+
+![Withdrawal 2](Execution_Results/8.PNG)
+
+ - Error
+ 
+![Withdrawal error](Execution_Results/9.PNG)
---
+++ hw_m20_joint_savings/joint_savings.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.0;
+
+contract jointSavings {
+    
+    address payable accountOne;
+    address payable accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+    
+    constructor(address payable  _accountOne, address payable _accountTwo) public {
+        accountOne = _accountOne;
+        accountTwo = _accountTwo;
+    }
+    
+    function withdraw(uint amount, address payable recipient) public {
+        
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
+        
+        require(amount <= contractBalance, "Insufficient funds!");
+        
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
+        
+        recipient.transfer(amount);
+        
+        lastWithdrawAmount = amount;
+        
+        contractBalance = address(this).balance;
+    }
+    
+    function deposit() public payable {
+        
+        contractBalance = address(this).balance;
+    }
+    
+    function setAccounts(address payable account1, address payable account2) public {
+        
+        accountOne = account1;
+        accountTwo = account2;
+    }
+    
+    function() external payable {}
+}
